57810bb113f6dc0336b267b5a6a9d5e2cf88e15d
pro-30 <37265520+pro-30@users.noreply.github.com>
2021-02-16 13:53:18 +0530

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -149,5 +149,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'pr7shantzizou@gmail.com'
-EMAIL_HOST_PASSWORD = '3093@Pro'
+EMAIL_HOST_PASSWORD = '******'
 EMAIL_USE_TLS= True
